5d147b8b4d315b1c0f63893dd41dcdbc34546ed8
Oren Mordechai <orenmordechai@gmail.com>
2019-11-01 00:35:28 -0400

    fetching ingredients asynchronously

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -8,9 +8,8 @@ import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
-import axios from '../../axios-orders';
 import * as BurgerBuilderActions from '../../store/actions/index';
-
+import axios from '../../axios-orders';
 
 
 class BurgerBuilder extends Component {
@@ -20,20 +19,11 @@ class BurgerBuilder extends Component {
     // }
 
     state = {
-        purchasing: false,
-        loading: false,
-        error: null
+        purchasing: false
     }
 
     componentDidMount() {
         console.log(this.props);
-        axios.get('https://react-my-burger-43990.firebaseio.com/ingredients.json')
-            .then(response => {
-                this.setState({ingredients: response.data})
-            })
-            .catch(error => {
-                this.setState({error: true});
-            });
     }
 
     updatePurchaseState(ingredients) {
@@ -92,10 +82,6 @@ class BurgerBuilder extends Component {
                 purchaseContinued={this.purchaseContinueHandler} />;
         }     
         
-        
-        if(this.state.loading) {
-            orderSummary = <Spinner />;
-        }
         
         return (
             <Aux>
--- src/store/actions/burgerBuilder.js
+++ src/store/actions/burgerBuilder.js
@@ -1,4 +1,5 @@
 import * as actionTypes from './actionTypes';
+import axios from '../../axios-orders';
 
 export const addIngredient = (name) => {
     return {
@@ -12,4 +13,31 @@ export const removeIngredient = (name) => {
         type: actionTypes.REMOVE_INGREDIENT,
         ingredientName: name
     };
+};
+
+export const setIngredients = (ingredients) => {
+    return {
+        type: actionTypes.SET_INGREDIENTS,
+        ingredients: ingredients
+    }
+};
+
+export const fetchIngredientsFailed = () => {
+    return {
+        type: actionTypes.FETCH_INGREDIENTS_FAILED
+    }
 }
+
+export const initIngredients = () => {
+  return dispatch => {
+    axios.get('https://react-my-burger-43990.firebaseio.com/ingredients.json')
+        .then(response => {
+            // this.setState({ingredients: response.data})
+            dispatch(setIngredients(response.data))
+        })
+        .catch(error => {
+            // this.setState({error: true});
+            dispatch(fetchIngredientsFailed());
+        });
+  };  
+};
